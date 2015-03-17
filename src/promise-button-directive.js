832065d8edfb4fd9cac7d205c56725a526dc364b
Erpheus <erpheus@gmail.com>
2015-03-17 15:41:16 +0100

    First simple implementation, copied from another project.

---
+++ src/promise-button-directive.js
@@ -0,0 +1,21 @@
+(function(){
+	
+	var module = angular.module('promise-button-directive',['promise-button-controller','templates-dist']);
+
+	module.directive('promiseButton', ['$compile', function($compile){
+		return {
+			scope: {
+				'promiseButton': '&' 
+			},
+			controller: 'PromiseButtonController',
+			restrict: 'A',
+			templateUrl: 'promise-button.tpl.html',
+			transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			link: function($scope, iElm, iAttrs, controller) {
+				iElm.bind('click', $scope.startAction);
+			}
+		};
+	}]);
+
+})();

77badab316b186df0d633fcd23581f0731471b9b
Erpheus <erpheus@gmail.com>
2015-04-06 20:27:20 +0200

    Unit test for when-promise directive.

---
+++ tests/spec/directives/when-promise-directive.js
@@ -0,0 +1,40 @@
+describe('Directive: when-promise', function() {
+  var element,
+  	  scope,
+      trigger = '',
+      action,
+      startAction;
+
+  beforeEach(module('promise-button'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
+    scope.deferred = function(){}
+
+    element = angular.element('<fake-parent><span when-promise="aaaa"></span></fake-parent>');
+
+    element.data('$PromiseButtonController', {});
+
+    var parent = $compile(element)(scope);
+    scope.$digest();
+    element = angular.element(parent.children()[0]);
+  }));
+
+  it ('should replace itself with an equivalent ng-switch-when', function(){
+    expect(element.attr('when-promise')).toBeUndefined();
+    expect(element.attr('ng-switch-when')).toBe('aaaa');
+  });
+
+  it ('should replace itself with a ng-switch-default if value is idle', inject(function($compile){
+    // setup
+    element = angular.element('<fake-parent><span when-promise="idle"></span></fake-parent>');
+    var parent = $compile(element)(scope);
+    scope.$digest();
+    element = angular.element(parent.children()[0]);
+
+    expect(element.attr('when-promise')).toBeUndefined();
+    expect(element.attr('ng-switch-default')).toBeDefined();
+
+  }));
+
+});

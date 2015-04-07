bc1ee41388bc6b7b013d0c819be8a1eea6332fc0
Erpheus <erpheus@gmail.com>
2015-04-07 04:19:20 +0200

    Improved promise-button directive's test. No stupid tests anymore (like it should create an ng-switch).

--- tests/spec/promise-button-directive.js
+++ tests/spec/promise-button-directive.js
@@ -1,11 +1,7 @@
 describe('Directive: promise-button', function() {
   var element,
   	  scope,
-      trigger = '',
-      action,
       startAction;
-
-  var SAMPLETEXT="lalalalalalala";
 
   beforeEach(module('promise-button'));
 
@@ -23,43 +19,43 @@ describe('Directive: promise-button', function() {
     scope = $rootScope.$new();
     scope.deferred = function(){}
 
-    element = '<a promise-button="deferred">'+SAMPLETEXT+'</a>';
+    element = '<a promise-button="deferred"></a>';
 
     element = $compile(element)(scope);
     scope.$digest();
   }));
 
-  it ('should create a switch on status', function(){
-    expect(element.find('ng-switch').length).toBe(1);
-    expect(element.find('ng-switch').attr('on')).toBe('status');
-  });
-
-  it ('should transclude inside the switch', function(){
-    expect(element.find('ng-switch').html()).toMatch(SAMPLETEXT);
-  });
-
   describe('default trigger', function(){
 
     it ('should be binded to click by default', function(){
       element.triggerHandler('click');
-      expect(startAction.calls.count()).toEqual(1);
+      expect(startAction).toHaveBeenCalled();
+    });
+
+    it ('other events shouldn\'t trigger it', function(){
+      element.triggerHandler('dblclick');
+      expect(startAction).not.toHaveBeenCalled();
     });
 
   });
 
-  describe('custom trigger', function(){
+  describe('with custom trigger specified', function(){
 
-    it ('should be binded to promise-trigger attribute value if specified', inject(function($compile){
-
+    beforeEach(inject(function($compile){
       element = '<a promise-button="deferred" promise-trigger="dblclick">'+SAMPLETEXT+'</a>';
       element = $compile(element)(scope);
       scope.$digest();
-
-
-      element.triggerHandler('dblclick');
-      expect(startAction.calls.count()).toEqual(1);
     }));
 
+    it ('should be binded to promise-trigger attribute', function(){
+      element.triggerHandler('dblclick');
+      expect(startAction).toHaveBeenCalled();
+    });
+
+    it ('other events shouldn\'t trigger it', function(){
+      element.triggerHandler('click');
+      expect(startAction).not.toHaveBeenCalled();
+    });
 
   });
 

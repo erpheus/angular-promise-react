c134030d8a505ab0c6e809401f21154bd1d34a37
Erpheus <erpheus@gmail.com>
2015-04-07 04:52:52 +0200

    Updated promise-default test to stop using ng-switch and to mock sub directives properly.

--- tests/common.js
+++ tests/common.js
@@ -7,19 +7,32 @@ var STATES = {
 }
 
 
-function mockDirective(name){
-		angular.module('promise-button').config(function($provide){
-	    	$provide.decorator(name+'Directive', ['$delegate', function($delegate) {
-	        	//$delegate is array of all ng-click directive
-	        	//in this case first one is angular buildin ng-click
-	        	//so we remove it.
-	        	$delegate.shift();
-	        	return $delegate;
-	    	}]);
-		});
-		angular.module('promise-button').directive(name, function() {
-		    return {
-		        link: function() {}
-		    }
-		});
+function mockDirective(name, backups){
+	angular.module('promise-button').config(function($provide){
+    	$provide.decorator(name+'Directive', ['$delegate', function($delegate) {
+        	//$delegate is array of all {name} directive
+        	//in this case first one is angular buildin ng-click
+        	//so we remove it.
+        	backups[name] = $delegate.shift();
+        	return $delegate;
+    	}]);
+	});
+	angular.module('promise-button').directive(name, function() {
+	    return {
+	        link: function() {}
+	    }
+	});
+}
+
+function restoreDirective(name, backups){
+	angular.module('promise-button').config(function($provide){
+    	$provide.decorator(name+'Directive', ['$delegate', function($delegate) {
+        	//$delegate is array of all {name} directive
+        	//in this case first one is angular buildin ng-click
+        	//so we remove it.
+        	$delegate.pop();
+        	$delegate.unshift(backups[name]);
+        	return $delegate;
+    	}]);
+	});
 }
--- tests/spec/directives/promise-default-directive.js
+++ tests/spec/directives/promise-default-directive.js
@@ -1,42 +1,25 @@
 describe('Directive: promise-default', function() {
   var element,
-  	  scope;
+  	  scope,
+  	  SAMPLETEXT = 'transcluded text';
 
-  beforeEach(function(){mockDirective.apply(this,['ngSwitchDefault'])});
-  beforeEach(function(){mockDirective.apply(this,['ngSwitchWhen'])});
-
-
+  var backups = {};
+  beforeEach(function(){mockDirective.apply(this,['whenPromise', backups])});
+  afterEach(function(){restoreDirective.apply(this,['whenPromise', backups])});
 
   beforeEach(module('promise-button'));
 
   beforeEach(inject(function($rootScope, $compile) {
     scope = $rootScope.$new();
 
-    element = '<promise-default>Start</promise-default>';
+    element = '<promise-default>'+SAMPLETEXT+'</promise-default>';
 
     element = $compile(element)(scope);
     scope.$digest();
   }));
 
-  describe('default element', function(){
-
-  	var resulting_elements;
-
-  	beforeEach(function(){
-  		resulting_elements = element[0].querySelectorAll('[ng-switch-default]');
-  	});
-
-  	it('should exist', function(){
-  		expect(resulting_elements.length).toBe(1);
-  	});
-
-  	it('should have the text inside the original element', function(){
-  		expect(angular.element(resulting_elements[0]).text()).toBe('Start');
-  	});
-
-  });
-
   var states_and_checks = [
+  	{ state: STATES.IDLE, check: SAMPLETEXT},
   	{ state: STATES.LOADING, check : 'icon-spin' },
   	{ state: STATES.INTERMEDIATE, check : 'icon-spin' },
   	{ state: STATES.DONE, check: 'fa-check' },
@@ -48,7 +31,7 @@ describe('Directive: promise-default', function() {
   	var resulting_elements;
 
   	beforeEach(function(){
-  		resulting_elements = element[0].querySelectorAll('[ng-switch-when="'+state+'"]');
+  		resulting_elements = element[0].querySelectorAll('[when-promise="'+state+'"]');
   	});
 
   	it('should exist', function(){

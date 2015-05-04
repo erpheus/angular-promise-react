83dabdd3663e9cdbbb56a36c5d8be0d5e0c77fed
MBlanc <manuel.blanc@estudiante.uam.es>
2015-05-04 23:25:55 +0200

    Ran `grunt` to regenerate dist/ & demo/ files
    Deleted unused leftover in demo/ from the package rename

--- demo/angular-promise-react/angular-promise-react.js
+++ demo/angular-promise-react/angular-promise-react.js
@@ -1,18 +1,91 @@
 (function(){
-	
+	'use strict';
+
 	var module = angular.module('promise-react',['promise-react-templates']);
 
 	module.constant('STATES', {
-        IDLE: 'idle',
+		IDLE: 'idle',
 		LOADING: 'loading',
 		INTERMEDIATE: 'intermediate',
 		DONE: 'done',
 		FAILED: 'failed'
-    });
+	});
 
 })();
 (function(){
-	
+	'use strict';
+
+	var module = angular.module('promise-react');
+
+	module.factory('promiseButtonIf', ['ngIfDirective', function(ngIfDirective) {
+		var ngIf = ngIfDirective[0];
+		return function(additionalCondition) {
+			// Thanks to Joscha and hilnius (http://stackoverflow.com/q/20325480)
+			var reactIf = angular.extend({}, ngIf);
+
+			reactIf.scope = false;
+			reactIf.priority--;
+			reactIf.require = '^promiseButton';
+
+			// Clear the name
+			delete reactIf.name;
+			// The link property is used only if the compile property is not defined.
+			delete reactIf.compile;
+
+			reactIf.link = function(scope, iElement, iAttrs /*, promiseButtonController */) {
+				var initialNgIf = iAttrs.ngIf; // Initial ng-if attribute
+				var args = arguments, self = this;
+
+				iAttrs.ngIf = function() {
+					// If there was a initial ng-if attribute, test it
+					if (initialNgIf && !scope.$eval(initialNgIf)) return false;
+
+					// Invoke the additional condition with the link-time arguments
+					return additionalCondition.apply(self, args);
+				};
+
+				ngIf.link.apply(ngIf, args);
+			};
+
+			return reactIf;
+		};
+	}]);
+
+	// when-progress directive
+	module.directive('whenProgress', ['promiseButtonIf', 'STATES', function(promiseButtonIf, STATES) {
+		return promiseButtonIf(function(scope, iElement, iAttrs, promiseButtonController) {
+			var value = iAttrs.whenProgress;
+			if (value) {
+				// If a value is specified it matches only intermediate status with proper states
+				return promiseButtonController.status === STATES.INTERMEDIATE &&
+				       promiseButtonController.state  === scope.$eval(value);
+			}
+			else {
+				// If no value is specified it matches either loading or intermediate status
+				return promiseButtonController.status === STATES.INTERMEDIATE ||
+				       promiseButtonController.status === STATES.LOADING;
+			}
+		});
+	}]);
+
+	// when-progress directive
+	module.directive('whenPromise', ['promiseButtonIf', 'STATES', function(promiseButtonIf, STATES) {
+		return promiseButtonIf(function(scope, iElement, iAttrs, promiseButtonController) {
+			var value = iAttrs.whenPromise;
+			if (value) {
+				// If a value is specified it matches only intermediate status with proper states
+				return promiseButtonController.status === value;
+			} else {
+				// If no value is specified it matches either loading or intermediate status
+				return promiseButtonController.status !== STATES.IDLE;
+			}
+		});
+	}]);
+
+})();
+(function(){
+	'use strict';
+
 	var module = angular.module('promise-react');
 
 	module.directive('promiseDefault', function(){
@@ -27,113 +100,8 @@
 
 })();
 (function(){
-	
-	var module = angular.module('promise-react');
+	'use strict';
 
-	// thanks to Joscha and hilnius: http://stackoverflow.com/questions/20325480/angularjs-whats-the-best-practice-to-add-ngif-to-a-directive-programmatically
-	module.directive('whenProgress', ['ngIfDirective','STATES', function(ngIfDirective, STATES){
-		var ngIf = ngIfDirective[0];
-
-		return {
-			transclude: ngIf.transclude,
-        	priority: ngIf.priority - 1,
-        	terminal: ngIf.terminal,
-        	restrict: ngIf.restrict,
-        	require: '^promiseButton',
-        	link: function(scope, element, attributes, promiseButtonController) {
-        		var value = attributes['whenProgress'];
-        		// actual condition
-        		var progressCondition;
-
-        		if (value) { // If a value is specified it matches only intermediate status with proper states
-        			progressCondition = function(){
-	        			return promiseButtonController.status == STATES.INTERMEDIATE && promiseButtonController.state == scope.$eval(value);
-	        		}
-        		} else { // If no value is specified it matches either loading or intermediate status
-        			progressCondition = function(){
-	        			return promiseButtonController.status == STATES.INTERMEDIATE || promiseButtonController.status == STATES.LOADING;
-	        		}
-        		}
-
-
-	            // find the initial ng-if attribute
-	            var initialNgIf = attributes.ngIf, 
-	            	ifEvaluator;
-
-	            // if it exists, evaluates ngIf && condition
-	            if (initialNgIf) {
-	                ifEvaluator = function () {
-	                    return scope.$eval(initialNgIf) && progressCondition();
-	                }
-	            } else { // if there's no ng-if only condition
-	                ifEvaluator = function () {
-	                	//debugger;
-	                    return progressCondition();
-	                }
-	            }
-	            attributes.ngIf = ifEvaluator;
-	            ngIf.link.apply(ngIf, arguments);
-	        }
-
-		};
-	}]);
-
-})();
-(function(){
-	
-	var module = angular.module('promise-react');
-
-	// thanks to Joscha and hilnius: http://stackoverflow.com/questions/20325480/angularjs-whats-the-best-practice-to-add-ngif-to-a-directive-programmatically
-	module.directive('whenPromise', ['ngIfDirective','STATES', function(ngIfDirective, STATES){
-		var ngIf = ngIfDirective[0];
-
-		return {
-			transclude: ngIf.transclude,
-        	priority: ngIf.priority - 1,
-        	terminal: ngIf.terminal,
-        	restrict: ngIf.restrict,
-        	require: '^promiseButton',
-        	link: function(scope, element, attributes, promiseButtonController) {
-        		var value = attributes['whenPromise'];
-        		// actual condition
-        		var promiseCondition;
-
-        		if (value) { // If a value is specified it matches only intermediate status with proper states
-        			promiseCondition = function(){
-	        			return promiseButtonController.status == value;
-	        		}
-        		} else { // If no value is specified it matches either loading or intermediate status
-        			promiseCondition = function(){
-	        			return promiseButtonController.status != STATES.IDLE;
-	        		}
-        		}
-
-
-	            // find the initial ng-if attribute
-	            var initialNgIf = attributes.ngIf, 
-	            	ifEvaluator;
-
-	            // if it exists, evaluates ngIf && condition
-	            if (initialNgIf) {
-	                ifEvaluator = function () {
-	                    return scope.$eval(initialNgIf) && promiseCondition();
-	                }
-	            } else { // if there's no ng-if only condition
-	                ifEvaluator = function () {
-	                	//debugger;
-	                    return promiseCondition();
-	                }
-	            }
-	            attributes.ngIf = ifEvaluator;
-	            ngIf.link.apply(ngIf, arguments);
-	        }
-
-		};
-	}]);
-
-})();
-(function(){
-	
 	var module = angular.module('promise-react');
 
 	module.controller('PromiseButtonController', [
@@ -141,7 +109,7 @@
 		'STATES',
 		'$timeout',
 		function($scope, STATES, $timeout){
-			
+
 			var ctrl = this;
 			ctrl.action = null;
 			var controller = this;
@@ -168,40 +136,40 @@
 			setStatus(STATES.IDLE);
 
 			/* Called by the binding in the directive. Calls the action
-			 * specified and sets all the callbacks for handling the 
+			 * specified and sets all the callbacks for handling the
 			 * promise updates. */
 			ctrl.startAction = function(){
 				// Avoid duplicate calls
-				if ($scope.status != STATES.IDLE || !ctrl.action){
+				if ($scope.status !== STATES.IDLE || !ctrl.action){
 					return;
 				}
 				setStatus(STATES.LOADING);
 
 				/* Handler builder for the end of the promise */
-				var end = function(state) { return function(result){
-					setStatus(state, result);
+				var end = function(state) {
+					return function(result){
+						setStatus(state, result);
 
-					// Return to idle after timeout
-					$timeout(function(){
-						setStatus(STATES.IDLE);
-					},500);
-				}};
+						// Return to idle after timeout
+						$timeout(function(){
+							setStatus(STATES.IDLE);
+						},500);
+					};
+				};
 
 				var update = function(state){
 					setStatus(STATES.INTERMEDIATE, state);
-				}
-
-				//Button sometimes doesn't update.
-				$scope.$apply();
+				};
 
 				return ctrl.action($scope).then(end(STATES.DONE), end(STATES.FAILED), update);
-			}
+			};
 		}
 	]);
 
 })();
 (function(){
-	
+	'use strict';
+
 	var module = angular.module('promise-react');
 
 	module.directive('promiseButton', ['$parse', function($parse){
@@ -213,13 +181,19 @@
 				return function(scope, element, attr, ctrl) {
 					ctrl.action = fn;
 					var trigger = attr['promiseTrigger'] || 'click';
-					element.bind(trigger, ctrl.startAction);
-				}
+
+					element.bind(trigger, function() {
+						// Accesses to scope from outside a controller must
+						// be wrapped in an .apply() in order to take effect.
+						scope.$apply(ctrl.startAction);
+					});
+				};
 			}
 		};
 	}]);
 
 })();
+
 angular.module('promise-react-templates', ['directives/promise-default.tpl.html']);
 
 angular.module("directives/promise-default.tpl.html", []).run(["$templateCache", function($templateCache) {
